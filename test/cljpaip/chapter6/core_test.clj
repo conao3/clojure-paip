6f17ee9503332d64ec543798e411d36e8484f1b9
Naoya Yamashita <conao3@gmail.com>
2025-01-01 22:21:04 +0900

    change argument order

--- test/cljpaip/chapter6/core_test.clj
+++ test/cljpaip/chapter6/core_test.clj
@@ -5,24 +5,24 @@
 
 (t/deftest test-literal-match
   (t/is (= 1 1))
-  (t/is (= (sut/pat-match 42 42) {}))
-  (t/is (= (sut/pat-match 42 43) sut/fail))
-  (t/is (= (sut/pat-match :?x 42) {:?x 42})))
+  (t/is (= {} (sut/pat-match 42 42)))
+  (t/is (= sut/fail (sut/pat-match 42 43)))
+  (t/is (= {:?x 42} (sut/pat-match :?x 42))))
 
 (t/deftest test-sequential-pattern-match
-  (t/is (= (sut/pat-match [42 43] [42 43]) {}))
-  (t/is (= (sut/pat-match [:?x 43] [42 43]) {:?x 42}))
-  (t/is (= (sut/pat-match [:?x 43] [42 44]) sut/fail))
-  (t/is (= (sut/pat-match [[:?x] 43] [[42] 43]) {:?x 42})))
+  (t/is (= {} (sut/pat-match [42 43] [42 43])))
+  (t/is (= {:?x 42} (sut/pat-match [:?x 43] [42 43])))
+  (t/is (= sut/fail (sut/pat-match [:?x 43] [42 44])))
+  (t/is (= {:?x 42} (sut/pat-match [[:?x] 43] [[42] 43]))))
 
 (t/deftest test-segment-patterns-*
-  (t/is (= (sut/pat-match [[:?* :?x] 42] [42]) {:?x []}))
-  (t/is (= (sut/pat-match [[:?* :?x] 42] [7 42]) {:?x [7]}))
-  (t/is (= (sut/pat-match [[:?* :?x] 42] [7 8 42]) {:?x [7 8]}))
-  (t/is (= (sut/pat-match [[:?* :?x] 42] [7 8 43]) sut/fail)))
+  (t/is (= {:?x []} (sut/pat-match [[:?* :?x] 42] [42])))
+  (t/is (= {:?x [7]} (sut/pat-match [[:?* :?x] 42] [7 42])))
+  (t/is (= {:?x [7 8]} (sut/pat-match [[:?* :?x] 42] [7 8 42])))
+  (t/is (= sut/fail (sut/pat-match [[:?* :?x] 42] [7 8 43]))))
 
 (t/deftest test-segment-patterns-+
-  (t/is (= (sut/pat-match [[:?+ :?x] 42] [42]) sut/fail))
-  (t/is (= (sut/pat-match [[:?+ :?x] 42] [7 42]) {:?x [7]}))
-  (t/is (= (sut/pat-match [[:?+ :?x] 42] [7 8 42]) {:?x [7 8]}))
-  (t/is (= (sut/pat-match [[:?+ :?x] 42] [7 8 43]) sut/fail)))
+  (t/is (= sut/fail (sut/pat-match [[:?+ :?x] 42] [42])))
+  (t/is (= {:?x [7]} (sut/pat-match [[:?+ :?x] 42] [7 42])))
+  (t/is (= {:?x [7 8]} (sut/pat-match [[:?+ :?x] 42] [7 8 42])))
+  (t/is (= sut/fail (sut/pat-match [[:?+ :?x] 42] [7 8 43]))))
