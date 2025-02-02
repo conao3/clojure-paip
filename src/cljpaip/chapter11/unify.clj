727826bc876638bb7c068024b6bdcb33752c7a0a
conao3 <conao3@gmail.com>
2025-02-03 07:13:22 +0900

    add testcases

--- src/cljpaip/chapter11/unify.clj
+++ src/cljpaip/chapter11/unify.clj
@@ -35,15 +35,25 @@
 (defn unify
   ([x y]
    (unify x y {}))
-  ([x y bindings]
-   (cond
-     (nil? bindings) nil
-     (= x y) bindings
-     (variable? x) (unify-variable x y bindings)
-     (variable? y) (unify-variable y x bindings)
-     (and (sequential? x) (sequential? y))
-     (unify (next x) (next y)
-            (unify (first x) (first y) bindings)))))
+  ([x_ y_ bindings]
+   (let [;; when pattern is `(. ?x)`, convert into ?x.
+         ;; care about notation of cons-cell.
+         peal-cdr-fn #(cond-> %
+                        (and (seq? %) (= '. (first %)))
+                        ((fn [a]
+                           (when (not= 2 (count a))
+                             (throw (ex-info "exact 1 element after `.'" {})))
+                           (second a))))
+         x (peal-cdr-fn x_)
+         y (peal-cdr-fn y_)]
+     (cond
+       (nil? bindings) nil
+       (= x y) bindings
+       (variable? x) (unify-variable x y bindings)
+       (variable? y) (unify-variable y x bindings)
+       (and (sequential? x) (sequential? y))
+       (unify (next x) (next y)
+              (unify (first x) (first y) bindings))))))
 
 (defn unifier [x y]
   (subst-bindings (unify x y) x))
--- test/cljpaip/chapter11/unify_test.clj
+++ test/cljpaip/chapter11/unify_test.clj
@@ -3,69 +3,30 @@
    [clojure.test :as t]
    [cljpaip.chapter11.unify :as sut]))
 
-;; (sut/unify '(?x + 1) '(2 + ?y))
-;; {?x 2, ?y 1}
+(t/deftest unify-test
+  (t/is (= {'?x 2, '?y 1} (sut/unify '(?x + 1) '(2 + ?y))))
+  (t/is (= {'?x '?y} (sut/unify '?x '?y)))
+  (t/is (= {'?x '?y} (sut/unify '(?x ?x) '(?y ?y))))
+  (t/is (= {'?x '?y} (sut/unify '(?x ?x ?x) '(?y ?y ?y))))
+  (t/is (= {'?x '?y} (sut/unify '(?x ?y) '(?y ?x))))
+  (t/is (= {'?x '?y, '?y 'a} (sut/unify '(?x ?y a) '(?y ?x ?x))))
+  (t/is (nil? (sut/unify '?x '(f ?x))))
+  ;; (t/is (nil? (sut/unify '(?x ?y) '((f ?y) (f ?x))))) ;-> stack overflow
+  ;; (t/is (nil? (sut/unify '(?x ?y ?z) '((?y ?z) (?x ?z) (?x ?y))))) ;-> stack overflow
+  (t/is (= {} (sut/unify 'a 'a))))
 
-;; (sut/unify '(f ?x) '(f ?y))
-;; {?x ?y}
+(t/deftest unifier-test
+  (t/is (= '(0 + 0 = 0) (sut/unifier '(?a + ?a = 0) '(?x + ?y = ?y))))
+  (t/is (= '(2 + 2 = 2) (sut/unifier '(?a + ?a = 2) '(?x + ?y = ?y))))
+  (t/is (= '(a a a) (sut/unifier '(?x ?y a) '(?y ?x ?x))))
+  (t/is (= '((?a * 5 = 2) + (4 * 5) + 3) (sut/unifier '((?a * ?x = 2) + (?b * ?x) + ?c) '(?z + (4 * 5) + 3)))))
 
-;; (sut/unify '(?a + ?a = 0) '(?x + ?y = ?y))
-;; {?a ?x, ?x ?y, ?y 0}
+(t/deftest occurs-check-disabled-test
+  (binding [sut/*occurs-check* false]
+    (t/is (= '{?x (f ?x)} (sut/unify '?x '(f ?x))))
+    (t/is (= '{?x (f ?y) ?y (f ?x)} (sut/unify '(?x ?y) '((f ?y) (f ?x)))))
+    (t/is (= '{?x (?y ?z) ?y (?x ?z) ?z (?x ?y)} (sut/unify '(?x ?y ?z) '((?y ?z) (?x ?z) (?x ?y)))))))
 
-;; (sut/unifier '(?a + ?a = 0) '(?x + ?y = ?y))
-;; (0 + 0 = 0)
-
-;; (sut/unifier '(?a + ?a = 2) '(?x + ?y = ?y))
-;; (2 + 2 = 2)
-
-;; (sut/unify '(?x + 1) '(2 + ?y))
-;; {?x 2, ?y 1}
-
-;; (sut/unify '?x '?y)
-;; {?x ?y}
-
-;; (sut/unify '(?x ?x) '(?y ?y))
-;; {?x ?y}
-
-;; (sut/unify '(?x ?x ?x) '(?y ?y ?y))
-;; {?x ?y}
-
-;; (sut/unify '(?x ?y) '(?y ?x))
-;; {?x ?y}
-
-;; (sut/unify '(?x ?y a) '(?y ?x ?x))
-;; {?x ?y, ?y a}
-
-;; (sut/unify '?x '(f ?x)) ;; skip -> OK
-;; nil
-
-;; (sut/unify '(?x ?y a) '(?y ?x ?x))
-;; {?x ?y, ?y a}
-
-;; (sut/unify '?x '(f ?x))
-;; nil
-
-;; (sut/unify '(?x ?y) '((f ?y) (f ?x))) ;; stack overflow
-
-;; (sut/unify '(?x ?y ?z) '((?y ?z) (?x ?z) (?x ?y))) ;; so
-
-;; (sut/unify 'a 'a)
-;; {}
-
-;; (sut/unifier '(?x ?y a) '(?y ?x ?x))
-;; (a a a)
-
-;; (sut/unifier '((?a * ?x = 2) + (?b * ?x) + ?c) '(?z + (4 * 5) + 3))
-;; ((?a * 5 = 2) + (4 * 5) + 3)
-
-;; (with-bindings {#'sut/*occurs-check* false}
-;;   (sut/unify '?x '(f ?x)))
-;; {?x (f ?x)}
-
-;; (with-bindings {#'sut/*occurs-check* false}
-;;   (sut/unify '(?x ?y) '((f ?y) (f ?x))))
-;; {?x (f ?y), ?y (f ?x)}
-
-;; (with-bindings {#'sut/*occurs-check* false}
-;;   (sut/unify '(?x ?y ?z) '((?y ?z) (?x ?z) (?x ?y))))
-;; {?x (?y ?z), ?y (?x ?z), ?z (?x ?y)}
+(t/deftest cons-unify-test
+  (t/is '{?a 2 ?b 1 ?c (2 3)}
+        (sut/unify '(member 2 (1 2 3)) '(member ?a (?b . ?c)))))
